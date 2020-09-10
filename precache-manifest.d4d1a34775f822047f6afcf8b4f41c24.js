f662c8308ae656e8d5b8ab6a69e069a74fe8475a
Mikolaj <mikolaj35@wp.pl>
2020-09-10 20:37:38 +0200

    Updates

---
+++ precache-manifest.d4d1a34775f822047f6afcf8b4f41c24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cee6265de21d18b30bb586094b4370e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b5a3378e6cdf736f8c8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e56de1fdf05985455486",
+    "url": "/static/js/2.9477fcd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9477fcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5a3378e6cdf736f8c8",
+    "url": "/static/js/main.48910ec3.chunk.js"
+  },
+  {
+    "revision": "996f63dc324f83c0bb0d",
+    "url": "/static/js/runtime-main.db15bb72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
